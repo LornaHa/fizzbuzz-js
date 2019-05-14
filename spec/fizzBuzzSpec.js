561ab234da15f5f6f7135de252b73953c5c58ed7
Lorna Harwood <lornapeepwreckers@gmail.com>
2019-05-14 14:01:50 +0100

    divisble by 5 with passing tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -10,5 +10,28 @@ describe('FizzBuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it ('divisible by 5', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
   });
+
+  describe ('knows when a number is NOT', function() {
+
+    it ('divisible by 3', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it ('divisible by 5', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+  });
+
+
+
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,9 @@
 function FizzBuzz(){};
 
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
-  return true;
+  return (number % 3 == 0);
+};
+
+FizzBuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 == 0);
 };
